e9f1f1ef69d42bbdb7fb46b36553ac84bccaeacf
Giang Dao <giang.bdao@gmail.com>
2025-01-01 19:15:17 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const url = 'https://local-business-data.p.rapidapi.com/photo-details?business_id=0x89c259b5a9bd152b%3A0x31453e62a3be9f76&photo_id=AF1QipMPYCqZ5Fe8a7Jc51KT9pWOS5c6tOKY_xvkCl23';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '4b721a1bf8mshb0a02234777090bp1e97d5jsn1bb018b34d99',
+		'x-rapidapi-host': 'local-business-data.p.rapidapi.com'
+	}
+};
+
+
+async function yuryhandsome(){
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
